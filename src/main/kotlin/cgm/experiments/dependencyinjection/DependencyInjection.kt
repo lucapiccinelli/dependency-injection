ffc2fed8f09eab886ac51586dd32cc0a6b08e080
Luca Piccinelli <luca.picci@gmail.com>
2021-05-14 10:04:07 +0200

    implementation example

--- src/main/kotlin/cgm/experiments/dependencyinjection/DependencyInjection.kt
+++ src/main/kotlin/cgm/experiments/dependencyinjection/DependencyInjection.kt
@@ -1,14 +1,36 @@
 package cgm.experiments.dependencyinjection
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.jvm.jvmErasure
 
 object DependencyInjection {
-    inline fun <reified T> get(): T? {
-        TODO("Not yet implemented")
+    lateinit var clazz: KClass<Any>
+
+    inline fun <reified T: Any> get(): T? {
+        val emptyConstructor: KFunction<Any>? = clazz
+            .constructors
+            .firstOrNull { it.parameters.isEmpty() }
+
+        if(emptyConstructor == null){
+            val param: List<Any> = clazz.constructors.first().parameters.map {
+                it.type.jvmErasure
+                    .constructors
+                    .first { it.parameters.isEmpty() }
+                    .call()
+            }
+
+            val args: Array<Any> = param.toTypedArray()
+
+            return clazz.constructors.first().call(*args) as T?
+        }
+
+        return emptyConstructor.call() as T?
     }
 
-    inline fun <reified T> add() {
-        TODO("Not yet implemented")
+    @Suppress("UNCHECKED_CAST")
+    inline fun <reified T: Any> add() {
+        clazz = T::class as KClass<Any>
     }
 
     fun <T: Any> add(clazz: KClass<T>) {
@@ -29,7 +51,7 @@ object DependencyInjection {
     }
 
     fun reset() {
-        TODO("Not yet implemented")
+        println("reset")
     }
 }
 
